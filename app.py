cfd671e76019f61c1ab605da2e92c1780eddf9f2
Usama Mohammed Elareeny <uelareeny@gmail.com>
2026-04-29 16:19:06 +0300

    Fix(app.py): Add CORS middleware to FastAPI application
    Added CORS middleware to handle cross-origin requests.

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Adding middlware to solve the CORS problem for me
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://www.wakili.me/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def greet_json():
